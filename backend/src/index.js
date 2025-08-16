0aef4b6ddc9ba4b671cdd6278ffecad368adb61b
ardatak1992 <ardatak1992@gmail.com>
2025-08-17 01:08:51 +0300

    added cron to remove temp files automaticaly

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,8 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
-
+import fs from "fs";
+import cron from "node-cron";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -23,9 +24,6 @@ const PORT = process.env.PORT || 5000;
 
 const httpServer = createServer(app);
 initializeSocket(httpServer);
-
-
-
 
 app.use(
   cors({
@@ -48,12 +46,36 @@ app.use(
   })
 );
 
+const tempDir = path.join(process.cwd(), "tmp");
+
+// delete temp files every minute
+cron.schedule("0 * * * *", () => {
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 // error handler
 app.use((err, req, res, next) => {
@@ -69,5 +91,3 @@ httpServer.listen(PORT, () => {
   console.log("Server is running on port", PORT);
   connectDB();
 });
-
-
