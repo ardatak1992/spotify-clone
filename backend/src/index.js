160daf54c00db99d5c3e975e56a8d611f8de433e
ardatak1992 <ardatak1992@gmail.com>
2025-08-16 18:46:16 +0300

    initialized socket.io in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,12 +12,20 @@ import adminRoutes from "./routes/admin.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
+import { createServer } from "http";
+import { initializeSocket } from "./lib/socket.js";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const httpServer = createServer(app);
+initializeSocket(httpServer);
+
+
+
 
 app.use(
   cors({
@@ -57,7 +65,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log("Server is running on port", PORT);
   connectDB();
 });
