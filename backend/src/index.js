4093f696d60a5a81137690203021259a634fd849
ardatak1992 <ardatak1992@gmail.com>
2025-08-17 03:25:22 +0300

    fixed dist path

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,54 +1,54 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
 import fs from "fs";
+import { createServer } from "http";
 import cron from "node-cron";
+
+import { initializeSocket } from "./lib/socket.js";
+
+import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
-import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
-import { createServer } from "http";
-import { initializeSocket } from "./lib/socket.js";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 
-// to parse req.body
-app.use(express.json());
-app.use(clerkMiddleware()); // this will add auth to req obj
+app.use(express.json()); // to parse req.body
+app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
 app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: path.join(__dirname, "tmp"),
     createParentPath: true,
     limits: {
-      fileSize: 10 * 1024 * 1024, // 10 Mb
+      fileSize: 10 * 1024 * 1024, // 10MB  max file size
     },
   })
 );
 
+// cron jobs
 const tempDir = path.join(process.cwd(), "tmp");
-
-// delete temp files every minute
 cron.schedule("0 * * * *", () => {
   if (fs.existsSync(tempDir)) {
     fs.readdir(tempDir, (err, files) => {
@@ -64,18 +64,16 @@ cron.schedule("0 * * * *", () => {
 });
 
 app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontendDist = path.resolve(__dirname, "../frontend/dist");
-  console.log("Serving frontend from:", frontendDist);
-  app.use(express.static(frontendDist));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(frontendDist, "index.html"));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
@@ -90,6 +88,6 @@ app.use((err, req, res, next) => {
 });
 
 httpServer.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+  console.log("Server is running on port " + PORT);
   connectDB();
 });
